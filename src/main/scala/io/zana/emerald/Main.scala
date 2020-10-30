d2cd4ac04af66758521899d84988e1f548042ec4
zana-a <zana200.a@gmail.com>
2020-10-30 14:57:20 +0000

    add ast for definition

--- src/main/scala/io/zana/emerald/Main.scala
+++ src/main/scala/io/zana/emerald/Main.scala
@@ -1,29 +1,43 @@
 package io.zana.emerald
 
-import java.nio.file.{Files, Path}
+import scala.util.parsing.combinator._
 
-import scala.io.Source
+case class WordFreq(word: String, count: Int) {
+  override def toString = s"Word <$word> occurs with frequency $count"
+}
 
-object Main {
-
-
-  def main(args: Array[String]): Unit = {
-
-    if (args.length.isNaN) {
-      val input = args(0)
-
-      if (Files.exists(Path.of(input))) {
-        val input = Source.fromFile(args(0))
-        for (line <- input) {
-          print(line)
-        }
-        input.close()
-      } else {
-        print(s"The file: ${args(0)} was not found")
-      }
-    } else {
-      print("Please provide a file to run")
+class SimpleParser extends RegexParsers {
+  def word: Parser[String] =
+    """[a-z]+""".r ^^ {
+      _.toString
     }
 
+  def number: Parser[Int] =
+    """(0|[1-9]\d*)""".r ^^ {
+      _.toInt
+    }
+
+  def freq: Parser[WordFreq] = word ~ number ^^ { case wd ~ fr => WordFreq(wd, fr) }
+}
+
+object Main extends SimpleParser {
+  def main(args: Array[String]) = {
+    parse(freq, "word 121") match {
+      case Success(matched, _) => println(matched)
+      case Failure(msg, _) => println(s"FAILURE: $msg")
+      case Error(msg, _) => println(s"ERROR: $msg")
+    }
   }
 }
+
+
+// a = [1, 2, 3]
+
+// result of AST
+//  program ->
+//    identifier ->
+//      name -> "a"
+//        values -> [
+//          number -> 1
+//          number -> 2
+//        ]
