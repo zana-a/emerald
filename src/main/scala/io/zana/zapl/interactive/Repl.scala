ab8abdecf5f7fea95504a33cfdce7363a980623f
zana-a <zana200.a@gmail.com>
2020-12-21 21:24:13 +0000

    add quit to repl

--- src/main/scala/io/zana/zapl/interactive/Repl.scala
+++ src/main/scala/io/zana/zapl/interactive/Repl.scala
@@ -7,19 +7,21 @@ import scala.io.StdIn
 
 object Repl {
 
-  def start: Unit = {
-
-    while (true) {
-
-      val input = StdIn.readLine("zapl>")
-
+  def loop: Unit = {
+    val input = StdIn.readLine(">")
+    if (input != "::exit") {
       Parser.parseAll(Parser.program, input) match {
         case Parser.Success(result, _) => println(result)
         case Parser.Failure(msg, _) => println(s"Fail: ${msg}")
         case Parser.Error(msg, _) => throw new Error(s"Erorr: ${msg}")
       }
-
-      start
+      loop
+    } else {
+      println("Goodbye!")
     }
+  }
+
+  def start: Unit = {
+    loop
   }
 }
