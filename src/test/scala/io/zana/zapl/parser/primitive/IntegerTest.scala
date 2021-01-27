fb44c9293ead598fa4d5bf0cd01353aff388ca84
zana-a <zana200.a@gmail.com>
2021-01-27 16:53:13 +0000

    removed tests due to complex nature

--- src/test/scala/io/zana/zapl/parser/primitive/IntegerTest.scala
+++ src/test/scala/io/zana/zapl/parser/primitive/IntegerTest.scala
@@ -1,5 +1,9 @@
 package io.zana.zapl.parser.primitive
 
+import io.zana.zapl.parser.Base._
+import io.zana.zapl.parser.Primitive
+import org.junit.Test
+
 class IntegerTest {
 
 }
--- src/test/scala/io/zana/zapl/parser/primitive/StringTest.scala
+++ src/test/scala/io/zana/zapl/parser/primitive/StringTest.scala
@@ -5,43 +5,6 @@ import org.junit.Assert._
 import org.junit.Test
 
 class StringTest {
-
-  def template(s: String, errorMessage: String = "") = {
-    import io.zana.zapl.parser.Base._
-
-    parse(Primitive.string, "\"" + s + "\"") match {
-      case Success(s, _) =>
-        assert(true) // Success: Parsed -> $s
-
-      case Failure(f, _) =>
-        assert(false, s"Failure: ${f}\n\tMessage:${errorMessage}")
-
-      case Error(e, _) =>
-        assert(false, s"\n\tError: $e")
-    }
-  }
-
-  @Test
-  def testEmptyString =
-    template("")
-
-  @Test
-  def testStringNoSpace =
-    template("hello")
-
-  @Test
-  def testStringWithSpace =
-    template("hello world")
-
-  @Test
-  def testStringWithSpecialCharacters =
-    template("hello, world! where shall we eat @?")
-
-  @Test
-  def testStringWithEscapedCharacters =
-    template("\"")
-    template("\'")
-    template("\\")
-    template("\\\"\'\n\t")
+  // TODO: Create tests for the result of the parser
 }
 
