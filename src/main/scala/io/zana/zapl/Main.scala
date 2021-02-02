d82d86f20829067c948e36d820434e9d0eef894b
zana-a <zana200.a@gmail.com>
2021-02-02 16:06:33 +0000

    clean up runner

--- src/main/scala/io/zana/zapl/Main.scala
+++ src/main/scala/io/zana/zapl/Main.scala
@@ -1,20 +1,5 @@
 package io.zana.zapl
 
-import pprint.pprintln
-
-object Main {
-
-  def main(args: Array[String]): Unit = {
-
-    if (args.length > 0) {
-      pprintln(
-        Runner.fromFile(args(0)),
-        width = 2,
-        indent = 2,
-      )
-    } else {
-      println("No input given")
-    }
-  }
+object Main extends App {
+  Runner.runFromFile(args)
 }
-
--- src/main/scala/io/zana/zapl/Runner.scala
+++ src/main/scala/io/zana/zapl/Runner.scala
@@ -2,23 +2,40 @@ package io.zana.zapl
 
 import io.zana.zapl.parser.Base
 import io.zana.zapl.parser.program.Program
+import pprint.pprintln
 
 import scala.io.Source
 
 object Runner {
 
-  def fromFile(path: String): Base.ParseResult[Any] = {
-    val io = Source.fromFile(path)
-    val source = io.mkString
-    io.close
-    parse(source)
+  private def format(inner: Base.ParseResult[Any]): Unit = pprintln(inner)
+
+  object Parser {
+
+    private def parse(input: String): Base.ParseResult[Any] = {
+      Base.parseAll(Program.build, input)
+    }
+
+    def fromSource(input: String): Base.ParseResult[Any] = {
+      parse(input)
+    }
+
+    def fromFile(path: String): Base.ParseResult[Any] = {
+      val io = Source.fromFile(path)
+      val source = io.mkString
+      io.close
+      parse(source)
+    }
   }
 
-  def fromSource(input: String): Base.ParseResult[Any] = {
-    parse(input)
+  def runFromFile(args: Array[String]): Unit = {
+    args.length match {
+      case 0 => println("No input file given!")
+      case 1 | _ => format(Parser.fromFile(args(0)))
+    }
   }
 
-  private def parse(input: String): Base.ParseResult[Any] = {
-    Base.parseAll(Program.build, input)
+  def runFromSource(input: String): Unit = {
+    format(Parser.fromSource(input))
   }
 }
