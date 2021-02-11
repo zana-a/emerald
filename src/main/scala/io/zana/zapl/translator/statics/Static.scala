48b7ecbc871f7b1541c63a04f3a180f8d4fe9cf7
zana-a <zana200.a@gmail.com>
2021-02-11 18:34:39 +0000

    fixed issue with wrong static

--- src/main/scala/io/zana/zapl/translator/statics/Static.scala
+++ src/main/scala/io/zana/zapl/translator/statics/Static.scala
@@ -8,8 +8,8 @@ object Static extends Translatable[statics.Static.Static]{
   override def translate(structure: statics.Static.Static): String = {
     structure match {
       case statics.Static.Int => "Int"
-      case statics.Static.String => "Int"
-      case statics.Static.Boolean => "Int"
+      case statics.Static.String => "String"
+      case statics.Static.Boolean => "Boolean"
       case statics.Static.Unit => "Unit"
       case statics.Static.Any => "Any"
       case statics.Static.List(generic) => s"List[$generic]"
