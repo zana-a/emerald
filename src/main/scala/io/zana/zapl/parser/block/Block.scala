f76671dc0237e3f261a6823d3698308437dc1557
zana-a <zana200.a@gmail.com>
2021-02-12 19:15:17 +0000

    fix calls

--- src/main/scala/io/zana/zapl/parser/block/Block.scala
+++ src/main/scala/io/zana/zapl/parser/block/Block.scala
@@ -1,20 +1,24 @@
 package io.zana.zapl.parser.block
 
 import io.zana.zapl.parser.base.Base._
+import io.zana.zapl.parser.call.Call
 import io.zana.zapl.parser.comment.LineComment
+import io.zana.zapl.parser.control.Control
+import io.zana.zapl.parser.expression.Expression
 import io.zana.zapl.parser.keyword.Keyword._
 import io.zana.zapl.parser.util.Parsable
+import io.zana.zapl.parser.variable.Variable
 import io.zana.zapl.structure.block.{Block => Structure}
 
 object Block extends Parsable[Structure] {
 
   override def apply: Parser[Structure] = {
     DO ~> rep(
-      LineComment
-        | variable
-        | call
-        | expression
-        | control
+      LineComment.apply
+        | Variable.apply
+        | Call.apply
+        | Expression.apply
+        | Control.apply
     ) <~ END ^^ (values => Structure(values))
   }
 }
--- src/main/scala/io/zana/zapl/parser/variable/Variable.scala
+++ src/main/scala/io/zana/zapl/parser/variable/Variable.scala
@@ -1,22 +1,12 @@
 package io.zana.zapl.parser.variable
 
-import io.zana.zapl.parser.base.Base
 import io.zana.zapl.parser.base.Base._
-import io.zana.zapl.parser.keyword.Keyword._
-import io.zana.zapl.parser.primitive.Primitive
 import io.zana.zapl.parser.util.Parsable
-import io.zana.zapl.structure.common.Identifier
-import io.zana.zapl.structure.function._
 import io.zana.zapl.structure.variable.{Variable => Structure}
 
 object Variable extends Parsable[Structure] {
 
-  override def apply: Base.Parser[Structure] = {
-    def id: Parser[Identifier] = identifier <~ EQ
-
-    def body: Parser[FunctionBody] = Primitive.apply | call | expression
-
-    id ~ body ^^ { case id ~ body => Structure(id, body) }
-  }
+  override def apply: Parser[Structure] =
+    ???
 
 }
