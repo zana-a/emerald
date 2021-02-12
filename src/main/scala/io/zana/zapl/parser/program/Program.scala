86e5e53f79bbf029715fa29a8e0f34e78823c29f
zana-a <zana200.a@gmail.com>
2021-02-12 20:46:33 +0000

    fix Module

--- src/main/scala/io/zana/zapl/parser/program/Program.scala
+++ src/main/scala/io/zana/zapl/parser/program/Program.scala
@@ -2,14 +2,14 @@ package io.zana.zapl.parser.program
 
 import io.zana.zapl.parser.base.Base._
 import io.zana.zapl.parser.comment.LineComment
-import io.zana.zapl.parser.module.Module._
+import io.zana.zapl.parser.module.Module
 import io.zana.zapl.structure.program.{Program => Structure}
 
 object Program {
 
   def program: Parser[Structure] = rep(
     LineComment.apply
-      | module
+      | Module.apply
       | call
       | function
       | variable
