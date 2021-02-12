943575a502e6145498ccdec8487be9d61070f026
zana-a <zana200.a@gmail.com>
2021-02-12 18:35:38 +0000

    add variable parser

--- src/main/scala/io/zana/zapl/parser/variable/Variable.scala
+++ src/main/scala/io/zana/zapl/parser/variable/Variable.scala
@@ -1,20 +1,23 @@
 package io.zana.zapl.parser.variable
 
+import io.zana.zapl.parser.Parsable
+import io.zana.zapl.parser.base.Base
 import io.zana.zapl.parser.base.Base._
-import io.zana.zapl.parser.call.Call._
 import io.zana.zapl.parser.expression.Expression._
 import io.zana.zapl.parser.keyword.Keyword._
-import io.zana.zapl.parser.primitive.Primitive._
+import io.zana.zapl.parser.primitive.Primitive
 import io.zana.zapl.structure.common.Identifier
 import io.zana.zapl.structure.function._
-import io.zana.zapl.structure.variable.{Variable => Result}
+import io.zana.zapl.structure.variable.{Variable => Structure}
 
-object Variable {
+object Variable extends Parsable[Structure] {
 
-  def id: Parser[Identifier] = identifier <~ EQ
+  override def apply: Base.Parser[Structure] = {
+    def id: Parser[Identifier] = identifier <~ EQ
 
-  def body: Parser[FunctionBody] = `type` | call | expression
+    def body: Parser[FunctionBody] = Primitive.apply | call | expression
 
-  def variable: Parser[Result] =
-    id ~ body ^^ { case id ~ body => Result(id, body) }
+    id ~ body ^^ { case id ~ body => Structure(id, body) }
+  }
+
 }
